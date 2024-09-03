e0d6f12d3886232d412e3fb36756e217b3146caa
azaliaTan <tanaeva03@mail.ru>
2024-09-03 15:50:42 +0300

    2praktika

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//alert('hi'); простое выведение модал окна
+
+let name= prompt('введите ваше имя','user');
+alert(`Ваше имя ${name}`);
+
+
+let isTrue=confirm('Это ваше имя?');
+alert(isTrue);
